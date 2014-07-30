ee61b089b626c27d106fa36536948e2b6cd1606f
Samuel Loveland <samurai@samuelloveland.com>
2014-07-30 14:25:04 -0600

    filter results form

--- sams-app/js/min/controller-ck.js
+++ sams-app/js/min/controller-ck.js
@@ -1,1 +1,1 @@
-var myApp=angular.module("myApp",[]);myApp.controller("MyController",["$scope","$http",function(t,o){o.get("js/data.json").success(function(o){t.artists=o})}]);
+var myApp=angular.module("myApp",[]);myApp.controller("MyController",["$scope","$http",function(t,r){r.get("js/data.json").success(function(r){t.artists=r,t.artistOrder="name"})}]);
